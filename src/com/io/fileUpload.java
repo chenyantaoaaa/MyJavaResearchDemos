f9ffbb919348b183783e85d5e205a0e587954d35
chenyantaoaaa <334632474@qq.com>
2017-11-14 18:13:43 +0800

    文件操作研究

--- src/com/io/fileUpload.java
+++ src/com/io/fileUpload.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
+import java.util.Map;
 
 /**
  * Created by chenyantao
@@ -17,13 +20,19 @@ import java.util.UUID;
  */
 @Controller
 @RequestMapping("/upload")
+@SessionAttributes("status")
 public class fileUpload {
+
+    //当利用表单提交文件的时候 想要提交后不跳转页面 可以使用 jquery-form.js
 
     @RequestMapping(value = {"/uploadFile"},method = {RequestMethod.POST})
     public void UploadFile(@RequestParam(value = "uploadFile", required = false) MultipartFile file, HttpServletRequest req){
-        System.out.println(file.getName());
+        //使用request对象的getSession()获取session，如果session不存在则创建一个
+        long size = file.getSize();
+        HttpSession session = req.getSession();
+        session.setAttribute(file.getOriginalFilename(),file.getSize());
         // //存放图片的物理路径
-        String file_path = "E:\\upload\\";
+        String file_path = "F:\\upload\\";
         String newFileName = file.getOriginalFilename();
         File newFile = new File(file_path,newFileName);
         if(!newFile.exists()){
@@ -37,10 +46,33 @@ public class fileUpload {
     }
 
     @RequestMapping(value = {"/getProgess"},method = {RequestMethod.GET})
-    public void getProgess(HttpServletRequest req){
-        File f= new File("E:\\upload\\test.txt");
+    public @ResponseBody String getProgess(HttpServletRequest req,String fileName){
+        HttpSession session = req.getSession();
+        String ss [] = fileName.split("\\\\");
+        long allSize = (long)session.getAttribute(ss[ss.length-1]);
+        File f= new File("F:\\upload\\"+ss[ss.length-1]);
+        long nowSize = 0;
         if (f.exists() && f.isFile()){
-            System.out.println(f.length());
+            nowSize = f.length();
         }
+        String per = String.valueOf((nowSize/allSize)*100)+"%";
+        return per;
+    }
+
+    @RequestMapping(value = {"/progress"}, method = RequestMethod.POST )
+    @ResponseBody
+    public String initCreateInfo(Map<String, Object> model) {
+        Progress status = (Progress) model.get("status");
+        if(status==null){
+            return "{}";
+        }
+        String per = String.valueOf((status.getpBytesRead()/status.getpContentLength())*100)+"%";
+        return per;
+    }
+
+    public static void main(String[] args) {
+        String s = "F:\\upload\\test.txt";
+        String ss [] = s.split("\\\\");
+        System.out.println(ss[ss.length-1]);
     }
 }
--- web/UploadFile.jsp
+++ web/UploadFile.jsp
@@ -5,19 +5,20 @@
 <html>
 <head>
     <script type="text/javascript" src="js/jquery-1.9.1.js" ></script>
+    <script type="text/javascript" src="js/jquery-form.js" ></script>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <style type="text/css">
         body, td, div {font-size: 12px; font-familly: 宋体; }
         #progressBar {width: 400px; height: 12px; background: #FFFFFF; border: 1px solid #000000; padding: 1px; }
-        #progressBarItem {width: 1%; height: 100%; background: #FF0000; }
+        #progressBarItem {width: 0%; height: 100%; background: #FF0000; }
     </style>
 </head>
 <body>
-<form method="post" action="/upload/uploadFile.do" enctype="multipart/form-data">
+<form id="myForm" method="post" action="/upload/uploadFile.do" enctype="multipart/form-data">
     选择一个文件:
-    <input type="file" name="uploadFile" />
+    <input id="fileIn" type="file" name="uploadFile" />
     <br/><br/>
-    <input type="submit" value="上传" />
+    <input type="submit" value="上传" onclick="sub"/>
 </form>
 <div id="status">
     上传进度条：
@@ -25,23 +26,37 @@
     <div id="statusInfo"></div>
 </div>
 <script type="text/javascript">
-    var progress = 1;
-    setInterval("requestStatus()", 5000);
+    var finished = true;
+    var pro;
+    // 使用ajaxForm
+    $(function(){
+        /** 验证文件是否导入成功  */
+        $("#myForm").ajaxForm(function(data){
+        });
+    });
+
+    $("#myForm").submit(function(){
+        finished = false;
+        pro = setInterval("requestStatus()",1000);
+    });
+
+
+    var progress = "1";
     function requestStatus(){
-//        if(progress == 100){
-//            return
-//        }
-//        progress++;
-//        $('#progressBarItem').css("width",progress + "%");
         getProgess();
     }
 
     function getProgess(){
         $.ajax({
-            type:'get',
-            url:"localhost:8080/upload/getProgess.do",
+            type:'post',
+            url:"http://localhost:8080/upload/progress.do",
             success:function(data){//返回json结果
-                alert(data);
+                progress = data;
+                $('#progressBarItem').css("width",progress);
+                if(progress == "100%"){
+                    finished = true;
+                    clearInterval(pro);
+                }
             }});
     }
 </script>
